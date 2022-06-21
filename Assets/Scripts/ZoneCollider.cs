0a3452ddae6609cc2e5ae33933bf2dad89bbefba
Snifflaz <eisfreiesspeiseeis@gmail.com>
2022-06-21 12:09:45 +0200

    added Zonecollider script
    The zones now call a wwisevent when the player enters

---
+++ Assets/Scripts/ZoneCollider.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneCollider : MonoBehaviour
+{
+    /// <summary>
+    /// The event that will be played
+    /// </summary>
+    public AK.Wwise.Event ZoneWwiseEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "MainCamera" )
+        {
+            //Debug.Log("Entering new Zone");
+
+            if (ZoneWwiseEvent != null)
+            {
+                ZoneWwiseEvent.Post(gameObject);
+            }
+        }
+    }
+}
