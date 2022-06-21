d93e8326f60e4e51f38781e72134e3146105d1c3
Snifflaz <eisfreiesspeiseeis@gmail.com>
2022-06-21 12:52:55 +0200

    Finished Grounding technical prototype
    Will be improved later with actual models + animation. Also, we'll need to adjust the timings.

--- Assets/Scripts/GroundingTouch.cs
+++ Assets/Scripts/GroundingTouch.cs
@@ -8,30 +8,60 @@ public class GroundingTouch : MonoBehaviour, IMixedRealityTouchHandler
     /// <summary>
     /// Is true if the player is currently touching the object
     /// </summary>
-    private bool playerIsTouching = false; 
+    private bool playerIsTouching = false;
+
+    /// <summary>
+    /// a timer in secons
+    /// </summary>
+    private float timer = 0f;
+
+    /// <summary>
+    /// the time between two flowers' growth animation in seconds
+    /// </summary>
+    public float interval = 0.1f;
+
+    private Animator[] flowerAnimators;
+
+    private int iterator = 0;
+
+    private bool animationCompleted = false;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        flowerAnimators = GetComponentsInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!animationCompleted && playerIsTouching)
+        {
+            timer += Time.deltaTime;
+            if (timer >= interval)
+            {
+                if(iterator < flowerAnimators.Length)
+                {
+                    flowerAnimators[iterator].SetBool("Completed", true);
+                    iterator++;
+                }
+                else
+                {
+                    animationCompleted = true;
+                }
+                timer -= interval;
+            }
+        }
     }
 
     public void OnTouchCompleted(HandTrackingInputEventData eventData) 
     {
         playerIsTouching = false;
-        Debug.Log("[Debug] Touch completed");
     }
 
     public void OnTouchStarted(HandTrackingInputEventData eventData)
     {
         playerIsTouching = true;
-        Debug.Log("[Debug] Touch started");
     }
 
     public void OnTouchUpdated(HandTrackingInputEventData eventData) { }
