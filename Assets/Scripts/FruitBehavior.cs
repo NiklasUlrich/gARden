f452053675534b154a11c6a0047c2bcddc731e4a
Snifflaz <eisfreiesspeiseeis@gmail.com>
2022-06-28 18:53:55 +0200

    Added Wwise Events to fruits and tinytrees

--- Assets/Scripts/FruitBehavior.cs
+++ Assets/Scripts/FruitBehavior.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FruitBehavior : MonoBehaviour
 {
+    /// <summary>
+    /// Which event will be played when touch is started
+    /// </summary>
+    public AK.Wwise.Event GrabWwiseEvent;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +22,15 @@ public class FruitBehavior : MonoBehaviour
 
     }
 
+    //is called when the fruit is grabbed
     public void OnRelease()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
         rb.isKinematic = false;
+
+        if (GrabWwiseEvent != null)
+        {
+            GrabWwiseEvent.Post(gameObject);
+        }
     }
 }
