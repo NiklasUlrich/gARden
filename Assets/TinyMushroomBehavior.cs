40ca85b7032d78dc83e9a6d15e2f50f3c5d86d4f
Snifflaz <eisfreiesspeiseeis@gmail.com>
2022-07-10 19:58:09 +0200

    Revert "Revert "Revert "changed fruits behavior"""
    This reverts commit 0047f91c4fbc252829ffec5b51ecf90e9dc2dc99.

--- Assets/TinyMushroomBehavior.cs
+++ Assets/TinyMushroomBehavior.cs
@@ -19,5 +19,6 @@ public class TinyMushroomBehavior : MonoBehaviour
     public void OnGrab()
     {
         gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        Debug.Log("Tiny Mushroom picked up");
     }
 }
